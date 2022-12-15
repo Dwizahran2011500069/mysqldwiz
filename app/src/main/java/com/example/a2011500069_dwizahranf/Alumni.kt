c840a68cd816f348541ab94e556033c2a6e11bfa
Dwizahran2011500069 <2011500069@mahasiswa.atmaluhur.ac.id>
2022-12-15 21:42:58 +0700

    projectmysqlmpl

---
+++ app/src/main/java/com/example/a2011500069_dwizahranf/Alumni.kt
@@ -0,0 +1,12 @@
+package com.example.a2011500069_dwizahranf
+
+class Alumni(
+    val nim: String,
+    val nmAlumni: String,
+    val prodi: String,
+    val tmptLahir: String,
+    val tglLahir: String,
+    val alamat: String,
+    val noHp: String,
+    val thnLulus: Int
+)
